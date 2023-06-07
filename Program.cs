6b2bab26ef3e2706f3477c2353bc7c31d8aa8bd9
errdyy <errdao@gmail.com>
2023-06-07 21:45:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 73824658;
+
+        Console.WriteLine("Исходное число: " + number);
+
+        int[] digits = GetDigits(number);
+        Array.Sort(digits);
+
+        int result = GetNumber(digits);
+
+        Console.WriteLine("Минимальное число: " + result);
+
+        Console.ReadLine();
+    }
+
+    static int[] GetDigits(int number)
+    {
+        List<int> digits = new List<int>();//чтоб не париться с размерностью массива
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            digits.Add(digit);
+            number /= 10;
+        }
+
+        return digits.ToArray();
+    }
+
+    static int GetNumber(int[] digits)
+    {
+        int result = 0;
+        int mult = 1;
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            result += digits[i] * mult;
+            mult *= 10;
+        }
+
+        return -1 * result;
+    }
+}
